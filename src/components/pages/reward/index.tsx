89a82c226d64725b7714d275673790ba159839b2
Bo_ <phanlakhon.ort@gmail.com>
2024-07-29 20:04:29 +0700

    Update

--- src/components/pages/reward/index.tsx
+++ src/components/pages/reward/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import { TitlePageUser } from "@/components/common";
+import { IoReload } from "react-icons/io5";
+import Image from "next/image";
 
 const RewardPage = () => {
   return (
@@ -9,7 +11,19 @@ const RewardPage = () => {
         <div className="lg:w-[58.33333333%] mx-auto">
           <TitlePageUser title="แลกของรางวัล" />
 
-          <div className="animated animatedFadeInUp fadeInUp"></div>
+          <div className="animated animatedFadeInUp fadeInUp">
+            <div className="flex justify-center items-center text-sm font-light gap-8 mt-6">
+              <Image
+                src={"/images/diamond.png"}
+                alt={""}
+                width={50}
+                height={45}
+                className="w-[50px]"
+              />
+              <p className="">เพชรของคุณ 0 </p>
+              <IoReload size={22} />
+            </div>
+          </div>
         </div>
       </div>
     </>
